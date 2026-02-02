d55dc35044f110fafcdab5de4a00142d66fb61cf
donggukang09 <childenergy2@gmail.com>
2026-02-02 22:32:40 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,21 +1,15 @@
-// HIQASC Platform Configuration
-// Google Sheets IDs
 const SHEETS_CONFIG = {
     repairBacklog: '111K9l8gt-14roqvynNFEJrT2aLsTYjU8gQBsKNiyFmI',
     supplies: '1vfnyawoFJnxFODMpmk_iGGwEOfTo0duc1-8VUyPyjcA',
     parts: '1RUH7EPhOUEVNWvZbizvuf0u3AnZ8K7mbMF-hAto5kKw',
     leave: '1cmMEbIkmEL629RT04hpUUgXddnSVLGo1YwRzBwfyzCY',
     dailyReport: '1eyZOi_yD76jijCV8yE5k-FThswFlNu5OxdMy0IRdlEs',
-    overtime: '12U0BzTsjxPfK3AZqQyY479ozC0g1T3Cxutj6XmN75-k',
-    wiki: '1yEwC43ULV1baQ7rYJ2He1wgkEEl00FNMgo7gBkD9auM'
+    overtime: '12U0BzTsjxPfK3AZqQyY479ozC0g1T3Cxutj6XmN75-k'
 };
 
-// Apps Script Web App URLs
 const APPS_SCRIPT_CONFIG = {
     suppliesRequest: 'https://script.google.com/macros/s/AKfycbzuZ0u1GJAnWh5LmHJqw43rjb2TrrNIxrnzowaHe1iO0uqctHRFkCEVaZRPYKIMxlZjMQ/exec',
     parts: 'https://script.google.com/macros/s/AKfycbxH-usbWLQYim1T32p9ClYSnXnSToLQgebuQMGv9Apz3ThDx8wZkqaEEant6EiUWTAI/exec',
     leave: 'https://script.google.com/macros/s/AKfycbymUluI5lJ6PYRlDrAUIvZ5yCE0wjD0WVy6fpC7HkYfl8JhQjVbqsnvDVt9QOgl0mnY/exec',
     overtime: 'https://script.google.com/macros/s/AKfycbymUluI5lJ6PYRlDrAUIvZ5yCE0wjD0WVy6fpC7HkYfl8JhQjVbqsnvDVt9QOgl0mnY/exec'
 };
-
-// DEVELOPED BY DONGGU KANG
