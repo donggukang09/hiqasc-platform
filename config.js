9b1317844404d843dc4b9293201d85b8b8cd86cb
donggukang09 <childenergy2@gmail.com>
2026-02-04 21:16:30 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,18 +1,15 @@
 const SHEETS_CONFIG = {
     repairBacklog: '111K9l8gt-14roqvynNFEJrT2aLsTYjU8gQBsKNiyFmI',
     supplies: '1vfnyawoFJnxFODMpmk_iGGwEOfTo0duc1-8VUyPyjcA',
-    parts: '1RUf7EPNJUEVNMv2bi2vur8u3An28K7mDMF-hAto5Kkw',
+    parts: '1RUH7EPhOUEVNWvZbizvuf0u3AnZ8K7mbMF-hAto5kKw',
     leave: '1cmMEbIkmEL629RT04hpUUgXddnSVLGo1YwRzBwfyzCY',
     dailyReport: '1eyZOi_yD76jijCV8yE5k-FThswFlNu5OxdMy0IRdlEs',
-    overtime: '12U0BzTsjxPfK3AZqQyY479ozC0g1T3Cxutj6XmN75-k',
-    machineQuantity: '111K9l8gt-14roqvynNFEJrT2aLsTYjU8gQBsKNiyFmI',
-    wiki: '1yEwC43ULV1baQ7rYJ2He1wgkEEl00FNMgo7gBkD9auM'
+    overtime: '12U0BzTsjxPfK3AZqQyY479ozC0g1T3Cxutj6XmN75-k'
 };
 
 const APPS_SCRIPT_CONFIG = {
     suppliesRequest: 'https://script.google.com/macros/s/AKfycbzuZ0u1GJAnWh5LmHJqw43rjb2TrrNIxrnzowaHe1iO0uqctHRFkCEVaZRPYKIMxlZjMQ/exec',
     parts: 'https://script.google.com/macros/s/AKfycbxH-usbWLQYim1T32p9ClYSnXnSToLQgebuQMGv9Apz3ThDx8wZkqaEEant6EiUWTAI/exec',
     leave: 'https://script.google.com/macros/s/AKfycbymUluI5lJ6PYRlDrAUIvZ5yCE0wjD0WVy6fpC7HkYfl8JhQjVbqsnvDVt9QOgl0mnY/exec',
-    overtime: 'https://script.google.com/macros/s/AKfycbymUluI5lJ6PYRlDrAUIvZ5yCE0wjD0WVy6fpC7HkYfl8JhQjVbqsnvDVt9QOgl0mnY/exec',
-    쟈ㅏㅑ: 'https://script.google.com/macros/s/AKfycbwHPmVKR7wGBg9Ksc_BEQGtcN8r6u88I67lwYd8DxE5yE3xY75VMFhHqqItFrV36_78XQ/exec'
+    overtime: 'https://script.google.com/macros/s/AKfycbymUluI5lJ6PYRlDrAUIvZ5yCE0wjD0WVy6fpC7HkYfl8JhQjVbqsnvDVt9QOgl0mnY/exec'
 };
